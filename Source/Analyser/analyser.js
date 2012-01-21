a9259b7a4cdb7e4af057b467ead0eb4c18aef538
Jerome de Lafargue <jerome.delafargue@gmail.com>
2012-01-21 19:27:56 +0000

    Updated analyser.js

--- Source/Analyser/analyser.js
+++ Source/Analyser/analyser.js
@@ -1,49 +1,80 @@
-load('../utils.js')
+load('../utils.js');
 
-var className = arguments[0];
-if (arguments[1]) {
-    load(arguments[1]);
-}
-else {
-    try {
-        var fileName = arguments[0];
-        fileName += ".js";
-        load(fileName);
-    }
-    catch (exception) {
-        print("--- ERROR loading file:");
-        print("if no file name is provided, the file <class name provided>.js must be in the current dir.")
-        quit();
-    }
-}
-
-// TODO : check whether name conflicts can occur with the loaded class
-var props = [];
-for(var m in this) {
-    props.push(this[m]);
-}
-
-var constructor;
-var methods = [];
-for (var j in props) {
-    if(props[j] && props[j].name === className){
-        constructor = props[j].valueOf();
-        var c = new (constructor)(4);
-        for(var m in c) {
-            if(typeof c[m] == "function") {
-                methods.push({name: m, func: c[m]});
+var Analyser =
+{
+    getClassInfo : function(global) {
+        
+        function getArguments(test) {
+            //print(test)
+            //return this.arguments;
+        }
+        
+        var arguments = global.arguments;
+        var className = arguments[0];
+        if (arguments[1]) {
+            load(arguments[1]);
+        }
+        else {
+            try {
+                var fileName = className;
+                fileName += ".js";
+                load(fileName);
+            }
+            catch (exception) {
+                print("--- ERROR loading file:");
+                print("if no file name is provided, the file <class name provided>.js must be in the current dir.")
+                quit();
             }
         }
-        for(var i in methods) {
-            if(methods[i].name === "multiplyRadius") {
-                var mult = methods[i].func;
-                mult.call(c,12);
-                print(c.radius);
+
+        var props = [];
+        for(var m in global) {
+            props.push({p: m, v: global[m]});
+        }
+
+        var classInfo = {};
+        var methods = [];
+        for (var j in props) {
+            if(props[j].v && props[j].v.name === className && props[j].v){
+                classInfo.ctr = props[j].v.valueOf();
+                
+                // an instance of the class under test needs to be created in order
+                // to retrieve the classe's method signatures
+                var c = new (classInfo.ctr)();
+                for(var m in c) {
+                    if(typeof c[m] == "function") {
+                        var f = function(a){};
+                        //print(getArguments.apply(f,[2]));
+                        methods.push({name: m, func: c[m]});
+                    }
+                }
             }
         }
+        classInfo.methods = methods;
+        return classInfo;
     }
+}   
+
+Analyser.classInfo = Analyser.getClassInfo(this);
+
+for (var i=0; i<5; i++) {
+    var testInstance = new (Analyser.classInfo.ctr)(i);
+    print(testInstance.radius);
+    print((Analyser.classInfo.methods[2].func).call(testInstance,Math.PI,2));
 }
 
-function MethodInfo(name,parameters){};
 
-function ClassInfo(name,constructor,methods){};
+/*for(var i in methods) {
+    if(methods[i].name === "multiplyRadius") {
+        var mult = methods[i].func;
+        mult.call(c,12);
+        print(c.radius);
+    }
+}*/
+
+
+//print(dump(Analyser))
+//var info = Analyser.analyse();
+//function MethodInfo(name,parameters){};
+
+//function ClassInfo(name,constructor,methods){};
