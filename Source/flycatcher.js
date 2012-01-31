39c9f62c8a7071a97283c74f8b1a124d4dd19eb1
Jerome de Lafargue <jerome.delafargue@gmail.com>
2012-01-31 20:21:38 +0000

    Converting flycatcher.js such that it runs on node.js as opposed to the d8 shell

--- Source/flycatcher.js
+++ Source/flycatcher.js
@@ -1,60 +1,56 @@
-load('utils.js');
+#!/usr/local/bin/node
 
-var Flycatcher = {};
+var dump = require('./utils.js').dump;
+var vm = require('vm');
+var fs = require('fs');
 
-Flycatcher.Analyser =
+Analyser =
 {
-    getClassInfo : function(global) {
+    getClassInfo : function() {
         
-        var arguments = global.arguments;
-        var className = arguments[0];
-        if (arguments[1]) {
-            load(arguments[1]);
+        var classInfo = {};
+        var arguments = process.argv;
+        classInfo.className = arguments[2];
+        var classContext = {};
+        if (arguments[3]) {
+
+            var code = fs.readFileSync(arguments[3],'utf8');
+            vm.runInNewContext(code,classContext);
         }
         else {
             try {
-                var fileName = className;
+                var fileName = classInfo.className;
                 fileName += ".js";
-                load(fileName);
+                var code = fs.readFileSync(fileName,'utf8');
+                vm.runInNewContext(code,classContext);
             }
             catch (exception) {
-                print("--- ERROR loading file:");
-                print("if no file name is provided, the file <class name provided>.js must be in the current dir.")
-                quit();
+                console.log("ERROR loading file:");
+                console.log("If no file name is provided, the file <class name provided>.js must be in the current dir.")
+                process.exit(1);
             }
         }
-
-        var props = [];
-        for(var m in global) {
-            props.push({p: m, v: global[m]});
-        }
-
-        var classInfo = {};
+        
         var methods = [];
-        for (var j in props) {
-            if(props[j].v && props[j].v.name === className){
-                var constructor = props[j].v.valueOf();
-                var parameters = [];
-                for (var i = 0; i<constructor.length; i++) {
-                    var type = Flycatcher.Analyser.inferType(constructor,i);
-                    parameters.push(type);
+        var constructor = classContext[classInfo.className];
+        var parameters = [];
+        for (var i = 0; i<constructor.length; i++) {
+            var type = Analyser.inferType(constructor,i);
+            parameters.push(type);
+        }
+        classInfo.ctr = {func: constructor, params: parameters};
+        // an instance of the class under test needs to be created in order
+        // to retrieve the classe's method signatures
+        var c = new (classInfo.ctr.func)();
+        for(var m in c) {
+            var member = c[m];
+            if(typeof member == "function") {
+                var paramTypes = [];
+                for (var i = 0; i<member.length; i++) {
+                    var type = Analyser.inferType(member,i);
+                    paramTypes.push(type);
                 }
-                classInfo.ctr = {func: constructor, params: parameters};
-                
-                // an instance of the class under test needs to be created in order
-                // to retrieve the classe's method signatures
-                var c = new (classInfo.ctr.func)();
-                for(var m in c) {
-                    var member = c[m];
-                    if(typeof member == "function") {
-                        var paramTypes = [];
-                        for (var i = 0; i<member.length; i++) {
-                            var type = Flycatcher.Analyser.inferType(member,i);
-                            paramTypes.push(type);
-                        }
-                        methods.push({name: m, func: c[m], paramTypes: paramTypes});
-                    }
-                }
+                methods.push({name: m, func: c[m], paramTypes: paramTypes});
             }
         }
         classInfo.methods = methods;
@@ -62,11 +58,13 @@ Flycatcher.Analyser =
     },
 
     inferType : function(func,param) {
+        //dump(func);
+        //dump(param);
         return "int";
     }
 }   
 
-Flycatcher.RandomDataGenerator =
+RandomDataGenerator =
 {
     generate : function(paramTypes) {
         function getRandomNumber() {
@@ -112,11 +110,11 @@ Flycatcher.RandomDataGenerator =
     }
 }
 
-Flycatcher.RandomTestGenerator = {};
-Flycatcher.RandomTestGenerator.generate = function(classInfo) {
+RandomTestGenerator = {};
+RandomTestGenerator.generate = function(classInfo) {
     MAX_SEQUENCE_LENGTH = 10;
     var testCase = {};
-    var parameters = Flycatcher.RandomDataGenerator.generate(classInfo.ctr.params);
+    var parameters = RandomDataGenerator.generate(classInfo.ctr.params);
     testCase.ctr = {func: classInfo.ctr.func, params: parameters};
     var methodSequence = [];
     randomSequenceLength = Math.ceil(Math.random()*MAX_SEQUENCE_LENGTH);
@@ -125,28 +123,27 @@ Flycatcher.RandomTestGenerator.generate = function(classInfo) {
         var method = classInfo.methods[randomMethod];
         methodSequence.push({name: method.name,
                              func: method.func,
-                             params: Flycatcher.RandomDataGenerator.generate(method.paramTypes)});
+                             params: RandomDataGenerator.generate(method.paramTypes)});
     }
     testCase.methodSequence = methodSequence;
     return testCase;
 }
 
-Flycatcher.Executor =
+Executor =
 {
     execute : function(testCase) {
-        print("Executing test case.........");
+        //console.log("Executing test case.........");
         var testObj = new (testCase.ctr.func)(testCase.ctr.params[0],testCase.ctr.params[1],testCase.ctr.params[2],testCase.ctr.params[3]);
         var methods = testCase.methodSequence;
         var result;
         for(var m=0; m<methods.length; m++) {
             result = methods[m].func.apply(testObj,methods[m].params);
         }
-        print();
         return result;
     }
 }
 
-Flycatcher.stringifyTestCases = function(testCases) {
+stringifyTestCases = function(testCases) {
     var string = "";
     for(var i = 0; i<testCases.length; i++) {
         var constructor = testCases[i].t.ctr;
@@ -189,18 +186,16 @@ Flycatcher.stringifyTestCases = function(testCases) {
     return string;
 }
 
-Flycatcher.Analyser.classInfo = Flycatcher.Analyser.getClassInfo(this);
+Analyser.classInfo = Analyser.getClassInfo(this);
 var validTestCases = [];
 for (var i=0; i<3; i++) {
-    var exploratoryTestCase = Flycatcher.RandomTestGenerator.generate(Flycatcher.Analyser.classInfo);
-    var res = Flycatcher.Executor.execute(exploratoryTestCase);
+    var exploratoryTestCase = RandomTestGenerator.generate(Analyser.classInfo);
+    var res = Executor.execute(exploratoryTestCase);
     if(res !== "FLYCATCHER_TEST_CRASH"){
         validTestCases.push({t: exploratoryTestCase,r: res});
     }
 };
 
-var testScenarios = Flycatcher.stringifyTestCases(validTestCases);
-var fileName = "Flycatcher_"+this.arguments[0]+".js";
-os.system("touch",[fileName]);
-dump(this)
-
+var testScenarios = stringifyTestCases(validTestCases);
+var fileName = "Flycatcher_"+Analyser.classInfo.className+".js";
+fs.writeFileSync(fileName,testScenarios);
